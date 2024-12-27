17532dd7a788a34829e22b436be5177b845084d7
warrenrhodes <webanalyse237@gmail.com>
2024-12-27 21:49:01 +0100

    Fix merge conflit

--- nature_gift_admin/middleware.ts
+++ nature_gift_admin/middleware.ts
@@ -1,30 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
     await auth.protect()
-  }
-  try {
-    // Get the token
-    const { getToken } = await auth()
-    const token = await getToken()
-
-    // Clone the request headers and add the Authorization header
-    const requestHeaders = new Headers(request.headers)
-    requestHeaders.set('Authorization', `Bearer ${token}`)
-
-    // Return the response with the modified headers
-    return NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    })
-  } catch (error) {
-    console.error('[Middleware] Auth error:', error)
-    return NextResponse.redirect(new URL('/auth/signin', request.url))
   }
 })
 

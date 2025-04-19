775d82e0991dfc9b2423668c23be3a9b31e1f2e4
warren rhodes <webanalyse237@gmail.com>
2025-04-19 10:53:58 +0100

    Update page.tsx

--- nature_gift_store/src/app/(root)/blogs/[slug]/page.tsx
+++ nature_gift_store/src/app/(root)/blogs/[slug]/page.tsx
@@ -84,7 +84,7 @@ export default async function BlogAdsDetailPage(props: Props) {
 
   return (
     <div className="min-h-screen bg-background">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <div className="px-2 lg:px-8 py-12">
         <Loader loading={<BlogSkeleton />}>
           <AdsBlogDetail
             blog={blog}

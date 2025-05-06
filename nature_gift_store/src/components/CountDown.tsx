1a00c067970e4490df501412399fdfc7e0ac837c
warren rhodes <webanalyse237@gmail.com>
2025-05-06 21:41:02 +0100

    Update CountDown.tsx

--- nature_gift_store/src/components/CountDown.tsx
+++ nature_gift_store/src/components/CountDown.tsx
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
 import { useLocalization } from '@/hooks/useLocalization'
 
 const CountdownTimer = () => {
